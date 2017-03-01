08fd78fe8d358a4abdaa224b67c62f09aea87d92
Anders Stubberup <Anders_Stubberup@hotmail.com>
2017-03-01 10:11:36 +0000

    Initial Commput (Code i got from teacher)

--- Tic Tac Toe/Tic Tac Toe.cpp
+++ Tic Tac Toe/Tic Tac Toe.cpp
@@ -1,11 +1,64 @@
-// Tic Tac Toe.cpp : Defines the entry point for the console application.
-//
+// Tic-Tac-Toe
+// Plays the game of tic-tac-toe against a human opponent
+// This code is incomplete.  Functions have been declared but not defined.
 
-#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 
+using namespace std;
 
+// global constants
+const char X = 'X';
+const char O = 'O';
+const char EMPTY = ' ';
+const char TIE = 'T';
+const char NO_ONE = 'N';
+
+//function prototypes
+void instructions();
+char askYesNo(string question);
+int askNumber(string question, int high, int low = 0);
+char humanPiece();
+char opponent(char piece);
+void displayBoard(const vector<char>& board);
+char winner(const vector<char>& board);
+bool isLegal(const vector<char>& board, int move);
+int humanMove(const vector<char>& board, char human);
+int computerMove(vector<char> board, char computer);
+void announceWinner(char winner, char computer, char human);
+
+// main function
 int main()
 {
+    int move;
+    const int NUM_SQUARES = 9;
+    vector<char> board(NUM_SQUARES, EMPTY);
+    
+    instructions();
+    char human = humanPiece();
+    char computer = opponent(human);
+    char turn = X;
+    displayBoard(board);
+    
+    while (winner(board) == NO_ONE)
+    {
+        if (turn == human)
+        {
+            move = humanMove(board, human);
+            board[move] = human;
+        }
+        else
+        {
+            move = computerMove(board, computer);
+            board[move] = computer;
+        }
+        displayBoard(board);
+        turn = opponent(turn);
+    }
+    
+    announceWinner(winner(board), computer, human);
+   
     return 0;
 }
-
