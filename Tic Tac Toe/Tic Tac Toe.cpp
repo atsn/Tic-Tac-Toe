e5da3c2dd84a45a9f152f73091e01e27ea270340
Anders Stubberup <Anders_Stubberup@hotmail.com>
2017-03-01 10:31:35 +0000

    have made askYesNo methods

--- Tic Tac Toe/Tic Tac Toe.cpp
+++ Tic Tac Toe/Tic Tac Toe.cpp
@@ -18,7 +18,7 @@ const char NO_ONE = 'N';
 
 //function prototypes
 void instructions();
-//char askYesNo(string question);
+char askYesNo(string question);
 //int askNumber(string question, int high, int low = 0);
 //char humanPiece();
 //char opponent(char piece);
@@ -37,6 +37,7 @@ int main()
     vector<char> board(NUM_SQUARES, EMPTY);
     
     instructions();
+	askYesNo("Are you an idiot");
     /*char human = humanPiece();
     char computer = opponent(human);
     char turn = X;
@@ -66,5 +67,18 @@ int main()
 
 void instructions()
 {
-	cout << "Welcome to the ultimate man-machine showdown: Tic-Tac-Toe.\n--where human brain is pit against silicon processor\n\nMake your move known by entering a number, 0-8. The number corresponts to the desired bord position, as illustrated: \n\n\t0 | 1 | 2\n\t---------\n\n\t3 | 4 | 5\n\t---------\n\n\t6 | 7 | 8\n\nPrepare yourself, human. The battle is about to begin.";
+	cout << "Welcome to the ultimate man-machine showdown: Tic-Tac-Toe.\n--where human brain is pit against silicon processor\n\nMake your move known by entering a number, 0-8. The number corresponts to the desired bord position, as illustrated: \n\n\t0 | 1 | 2\n\t---------\n\n\t3 | 4 | 5\n\t---------\n\n\t6 | 7 | 8\n\nPrepare yourself, human. The battle is about to begin.\n";
+}
+
+char askYesNo(string Question)
+{
+	char input = '0';
+	while (input != 'y' && input != 'n' && input != 'Y' && input != 'N')
+	{
+		cout << "\n"<<Question << "\nPlease enter Y for yes and N for no\n";
+		cin >> input;
+		cin.ignore();
+	}
+
+	return  tolower(input);
 }
