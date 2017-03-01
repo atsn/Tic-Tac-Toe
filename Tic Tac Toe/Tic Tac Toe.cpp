17df2e430091f6c36976ef170a06b63d190847f9
Anders Stubberup <Anders_Stubberup@hotmail.com>
2017-03-01 10:55:41 +0000

    Have made methods to let the player chose X or O and make the computer Chose the oposet and to display the bord

--- Tic Tac Toe/Tic Tac Toe.cpp
+++ Tic Tac Toe/Tic Tac Toe.cpp
@@ -20,8 +20,8 @@ const char NO_ONE = 'N';
 void instructions();
 char askYesNo(string question);
 //int askNumber(string question, int high, int low = 0);
-//char humanPiece();
-//char opponent(char piece);
+char humanPiece();
+char opponent(char piece);
 //void displayBoard(const vector<char>& board);
 //char winner(const vector<char>& board);
 //bool isLegal(const vector<char>& board, int move);
@@ -32,36 +32,35 @@ char askYesNo(string question);
 // main function
 int main()
 {
-    int move;
-    const int NUM_SQUARES = 9;
-    vector<char> board(NUM_SQUARES, EMPTY);
-    
-    instructions();
-	askYesNo("Are you an idiot");
-    /*char human = humanPiece();
-    char computer = opponent(human);
-    char turn = X;
-    displayBoard(board);
-    
-    while (winner(board) == NO_ONE)
-    {
-        if (turn == human)
-        {
-            move = humanMove(board, human);
-            board[move] = human;
-        }
-        else
-        {
-            move = computerMove(board, computer);
-            board[move] = computer;
-        }
-        displayBoard(board);
-        turn = opponent(turn);
-    }
-    
-    announceWinner(winner(board), computer, human);
+	int move;
+	const int NUM_SQUARES = 9;
+	vector<char> board(NUM_SQUARES, EMPTY);
+
+	instructions();
+	char human = humanPiece();
+	/*char computer = opponent(human);
+	char turn = X;
+	displayBoard(board);
+
+	while (winner(board) == NO_ONE)
+	{
+		if (turn == human)
+		{
+			move = humanMove(board, human);
+			board[move] = human;
+		}
+		else
+		{
+			move = computerMove(board, computer);
+			board[move] = computer;
+		}
+		displayBoard(board);
+		turn = opponent(turn);
+	}
+
+	announceWinner(winner(board), computer, human);
    */
-    return 0;
+	return 0;
 }
 
 
@@ -75,10 +74,31 @@ char askYesNo(string Question)
 	char input = '0';
 	while (input != 'y' && input != 'n' && input != 'Y' && input != 'N')
 	{
-		cout << "\n"<<Question << "\nPlease enter Y for yes and N for no\n";
+		cout << "\n" << Question << "\nPlease enter Y for yes and N for no\n";
 		cin >> input;
 		cin.ignore();
 	}
-
 	return  tolower(input);
+}
+char humanPiece()
+{
+	char answer = askYesNo("\nBefore I can start beating you, I need to know. Do you want to go first\n");
+	if (answer == 'y')
+	{
+		cout << "\nOk i let you start you puny human\n";
+		return 'X';
+	}
+	cout << "\nOk i will start, lets see of your inferior brain can follow\n";
+	return 'O';
+}
+
+char opponent(char piece)
+{
+	if (piece == X) return  O;
+	return X;
+}
+
+void displayBoard(const vector<char>& board)
+{
+	cout << "\n\t" << board[0] << " | " << board[1] << " | " << board[2] << "\n\t" << board[3] << " | " << board[4] << " | " << board[5] << "\n\t" << board[6] << " | " << board[7] << " | " << board[8];
 }
